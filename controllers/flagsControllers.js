074a20f2adf9de05f11645ef9eac99957ea80b8b
James Haley <jamestehaley0@gmail.com>
2019-10-31 09:10:13 +0000

    added postFlag logic

--- controllers/flagsControllers.js
+++ controllers/flagsControllers.js
@@ -12,7 +12,16 @@ exports.postFlag = (req, res, next) => {
   addFlag(req.body)
     .then(flag => {
       res.status(201).send({ flag });
-      // fetchAllRoutes({longitude:flag.longitude, latitude:flag.latitude}).then(()=>{})
+      fetchAllRoutes({
+        longitude: flag.longitude,
+        latitude: flag.latitude
+      }).then(routes => {
+        const junctions = routes.map(route => {
+          if (routeFlagger(flag, route)) {
+            return { flag_id: flag.flag_id, route_id: route.route_id };
+          }
+        });
+      });
     })
     .catch(next);
 };
