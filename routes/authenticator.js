c99f7a81a37a2f79d604903ad761ed1b26d35ece
Dave Ridealgh <davecodingstuff@gmail.com>
2019-11-05 23:44:21 +0000

    app now uses authenticator at route '/' on every request

--- routes/authenticator.js
+++ routes/authenticator.js
@@ -7,10 +7,10 @@ const cognitoExpress = new CognitoExpress({
   tokenExpiration: 3600000
 });
 authenticator.use((req, res, next) => {
-  let accessTokenFromClient = req.headers.accesstoken;
-  if (!accessTokenFromClient)
+  const { usertoken } = req.headers;
+  if (!usertoken)
     return res.status(401).send('Access token missing from header.');
-  cognitoExpress.validate(accessTokenFromClient, (err, response) => {
+  cognitoExpress.validate(usertoken, (err, response) => {
     if (err) return res.status(401).send(err);
     res.locals.user = response;
     next();
