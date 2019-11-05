6c357e19fa14be4e7b3c4644f9facb4900a8eba8
Dave Ridealgh <davecodingstuff@gmail.com>
2019-11-05 22:47:26 +0000

    LOOK INTO POST FLAGS CONTROLLER! seems to be fixed but some strange behaviour

--- routes/authenticator.js
+++ routes/authenticator.js
@@ -7,7 +7,6 @@ const cognitoExpress = new CognitoExpress({
   tokenExpiration: 3600000
 });
 authenticator.use((req, res, next) => {
-  console.log(req.headers);
   let accessTokenFromClient = req.headers.accesstoken;
   if (!accessTokenFromClient)
     return res.status(401).send('Access token missing from header.');
@@ -18,10 +17,4 @@ authenticator.use((req, res, next) => {
   });
 });
 
-// const aws_exports = {
-//   identityPoolId: 'eu-west-2:a79fa191-da7c-4c3b-bf78-3a22c1f5b693',
-//   region: '',
-//   userPoolId: '',
-//   userPoolWebClientId: 'c8u6m0a8edelct1bchvrhdl0v'
-// };
 module.exports = authenticator;
