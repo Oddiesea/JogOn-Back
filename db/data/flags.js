80a4f9f4c6f8c9a430d88076784728b8ca81cdb5
Dave Ridealgh <davecodingstuff@gmail.com>
2019-11-06 11:07:04 +0000

    ammended seed data to account for new user_id data type

--- db/data/flags.js
+++ db/data/flags.js
@@ -3,36 +3,36 @@ module.exports = [
     latitude: 53.8008,
     longitude: -1.5491,
     flag_type_id: 1,
-    user_id: 1
+    user_id: 'Dave'
   },
   {
     latitude: 53.8108,
     longitude: -1.5591,
     flag_type_id: 2,
-    user_id: 2
+    user_id: 'Dave'
   },
   {
     latitude: 53.8208,
     longitude: -1.5691,
     flag_type_id: 3,
-    user_id: 3
+    user_id: 'Dave'
   },
   {
     latitude: 53.8308,
     longitude: -1.5791,
     flag_type_id: 4,
-    user_id: 4
+    user_id: 'Dave'
   },
   {
     latitude: 53.795006484598,
     longitude: -1.54642358422279,
-    user_id: 1,
+    user_id: 'Dave',
     flag_type_id: 1
   },
   {
     latitude: 53.7949486570067,
     longitude: -1.54665358364582,
-    user_id: 1,
+    user_id: 'Dave',
     flag_type_id: 1
   }
 ];
